cbb23d044dacde9c436dc9dd6fb073705d62978a
Jeffreeyy <17844@ma-web.nl>
2015-03-30 22:09:22 +0200

    Uploaded the files from my local to GitHub

---
+++ SSSSS/Assets/Scripts/Buttons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+public string playButtonText;
+public Texture playButtonTexture;
+public string quitButtonText;
+public Texture quitButtonTexture;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			AutoFade.LoadLevel(1 ,3,1,Color.black);
+		}
+	}
+}
---
+++ SSSSS/Assets/Scripts/CollectableManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollectableManager : MonoBehaviour {
+
+	public Text collectableText;
+	private int nrOfTotalCollectables;
+	private int nrOfCollectedItems;
+
+	void Start () {
+		GameObject[] collectables = GameObject.FindGameObjectsWithTag("Collectable");
+		nrOfCollectedItems = 0;
+		UpdateUI();
+	}
+
+	public void AddCollectable()
+	{
+		nrOfCollectedItems++;
+		//Debug.Log("Je hebt " + nrOfCollectedItems + " van de " + nrOfTotalCollectables + " verzameld.");
+		UpdateUI();
+		if (nrOfCollectedItems == nrOfTotalCollectables)
+        {
+			//Application.LoadLevel("win");
+		}
+	}
+	void UpdateUI()
+	{
+		collectableText.text = "" + nrOfCollectedItems;
+	}
+}
